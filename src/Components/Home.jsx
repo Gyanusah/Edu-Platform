8b8d0a3cb0a16509c76e5f2bd0e700b3fe84db4a
Gyanendra Shah <shahgyanendra555@gmail.com>
2025-10-14 12:25:07 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HeroSection from "../Screen/HeroSection";
 import CourseCategories from "../Screen/CourseCategories";
 import FeaturedCourses from "../Screen/FeaturedCourses";
-import Features from "../Screen/Features"
+import Features from "../Screen/features"
 import SuccessStories from "../Screen/SuccessStories";
 import Stats from "../Screen/Stats";
 import Notices from "../Screen/Notices";
